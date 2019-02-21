6c01564bea8aff29729fd1d6e3aeed562ebf9ea1
Nguyen Lam <nguyenlam.dev.website@gmail.com>
2019-02-21 15:09:15 +0700

    Fix sidebar

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -1,3 +1,29 @@
+<?php
+if (Auth::user()->getRoleNames()[0] == 'admin') {
+    $routes = [
+        'home',
+        'user-admin.index',
+        'user-admin.create',
+        'role.index',
+        'permission.index',
+        'category.index',
+        'campaign.index',
+        'campaign.myCampaign',
+        'transaction-manager.index',
+        'transaction-manager.individual',
+        'transaction-manager.listRequestPaid'
+    ];
+} elseif (Auth::user()->getRoleNames()[0] == 'user') {
+    $routes = [
+        'home',
+        'category.index',
+        'campaign.index',
+        'campaign.myCampaign',
+        'transaction-manager.individual',
+    ];
+}
+
+?>
 <div class="page-sidebar-wrapper">
     <!-- END SIDEBAR -->
     <!-- DOC: Set data-auto-scroll="false" to disable the sidebar from auto scrolling/focusing -->
@@ -10,7 +36,8 @@
         <!-- DOC: Set data-auto-scroll="false" to disable the sidebar from auto scrolling/focusing -->
         <!-- DOC: Set data-keep-expand="true" to keep the submenues expanded -->
         <!-- DOC: Set data-auto-speed="200" to adjust the sub menu slide up/down speed -->
-        <ul class="page-sidebar-menu  page-header-fixed page-sidebar-menu-hover-submenu " data-keep-expanded="false" data-auto-scroll="true" data-slide-speed="200">
+        <ul class="page-sidebar-menu  page-header-fixed page-sidebar-menu-hover-submenu " data-keep-expanded="false"
+            data-auto-scroll="true" data-slide-speed="200">
             <li class="nav-item start active open">
                 <a href="{{ route('home') }}" class="nav-link nav-toggle">
                     <i class="icon-home"></i>
@@ -25,16 +52,20 @@
                     <span class="arrow"></span>
                 </a>
                 <ul class="sub-menu">
-                    <li class="nav-item  ">
-                        <a href="{{ route('user-admin.index') }}" class="nav-link ">
-                            <span class="title">Danh sách tài khoản</span>
-                        </a>
-                    </li>
-                    <li class="nav-item  ">
-                        <a href="{{ route('user-admin.create') }}" class="nav-link ">
-                            <span class="title">Tạo mới tài khoản</span>
-                        </a>
-                    </li>
+                    @if(in_array('user-admin.index',$routes))
+                        <li class="nav-item  ">
+                            <a href="{{ route('user-admin.index') }}" class="nav-link ">
+                                <span class="title">Danh sách tài khoản</span>
+                            </a>
+                        </li>
+                    @endif
+                    @if(in_array('user-admin.create',$routes))
+                        <li class="nav-item  ">
+                            <a href="{{ route('user-admin.create') }}" class="nav-link ">
+                                <span class="title">Tạo mới tài khoản</span>
+                            </a>
+                        </li>
+                    @endif
                 </ul>
             </li>
             <li class="nav-item  ">
@@ -44,16 +75,20 @@
                     <span class="arrow"></span>
                 </a>
                 <ul class="sub-menu">
-                    <li class="nav-item">
-                        <a href="{{ route('role.index') }}" class="nav-link ">
-                            <span class="title">Vai trò</span>
-                        </a>
-                    </li>
-                    <li class="nav-item ">
-                        <a href="{{ route('permission.index') }}" class="nav-link ">
-                            <span class="title">Quyền</span>
-                        </a>
-                    </li>
+                    @if(in_array('role.index',$routes))
+                        <li class="nav-item">
+                            <a href="{{ route('role.index') }}" class="nav-link ">
+                                <span class="title">Vai trò</span>
+                            </a>
+                        </li>
+                    @endif
+                    @if(in_array('permission.index',$routes))
+                        <li class="nav-item ">
+                            <a href="{{ route('permission.index') }}" class="nav-link ">
+                                <span class="title">Quyền</span>
+                            </a>
+                        </li>
+                    @endif
                 </ul>
             </li>
 
@@ -64,11 +99,13 @@
                     <span class="arrow"></span>
                 </a>
                 <ul class="sub-menu">
-                    <li class="nav-item  ">
-                        <a href="{{ route('category.index') }}" class="nav-link ">
-                            <span class="title">Dịch vụ</span>
-                        </a>
-                    </li>
+                    @if(in_array('category.index',$routes))
+                        <li class="nav-item  ">
+                            <a href="{{ route('category.index') }}" class="nav-link ">
+                                <span class="title">Dịch vụ</span>
+                            </a>
+                        </li>
+                    @endif
                 </ul>
             </li>
             <li class="nav-item  ">
@@ -78,16 +115,20 @@
                     <span class="arrow"></span>
                 </a>
                 <ul class="sub-menu">
-                    <li class="nav-item  ">
-                        <a href="{{ route('campaign.index') }}" class="nav-link ">
-                            <span class="title">Dach sách chiến dịch</span>
-                        </a>
-                    </li>
-                    <li class="nav-item  ">
-                        <a href="{{ route('campaign.myCampaign') }}" class="nav-link ">
-                            <span class="title">Chiến dịch của bạn</span>
-                        </a>
-                    </li>
+                    @if(in_array('campaign.index',$routes))
+                        <li class="nav-item  ">
+                            <a href="{{ route('campaign.index') }}" class="nav-link ">
+                                <span class="title">Dach sách chiến dịch</span>
+                            </a>
+                        </li>
+                    @endif
+                    @if(in_array('campaign.myCampaign',$routes))
+                        <li class="nav-item  ">
+                            <a href="{{ route('campaign.myCampaign') }}" class="nav-link ">
+                                <span class="title">Chiến dịch của bạn</span>
+                            </a>
+                        </li>
+                    @endif
                 </ul>
             </li>
 
@@ -98,21 +139,27 @@
                     <span class="arrow"></span>
                 </a>
                 <ul class="sub-menu">
-                    <li class="nav-item  ">
-                        <a href="{{ route('transaction-manager.index') }}" class="nav-link ">
-                            <span class="title">Dách sách giao dịch</span>
-                        </a>
-                    </li>
-                    <li class="nav-item  ">
-                        <a href="{{ route('transaction-manager.individual') }}" class="nav-link ">
-                            <span class="title">Giao dịch của tôi</span>
-                        </a>
-                    </li>
-                    <li class="nav-item  ">
-                        <a href="{{ route('transaction-manager.listRequestPaid') }}" class="nav-link ">
-                            <span class="title">Yêu cầu thanh toán</span>
-                        </a>
-                    </li>
+                    @if(in_array('transaction-manager.index',$routes))
+                        <li class="nav-item  ">
+                            <a href="{{ route('transaction-manager.index') }}" class="nav-link ">
+                                <span class="title">Dách sách giao dịch</span>
+                            </a>
+                        </li>
+                    @endif
+                    @if(in_array('transaction-manager.individual',$routes))
+                        <li class="nav-item  ">
+                            <a href="{{ route('transaction-manager.individual') }}" class="nav-link ">
+                                <span class="title">Giao dịch của tôi</span>
+                            </a>
+                        </li>
+                    @endif
+                    @if(in_array('transaction-manager.listRequestPaid',$routes))
+                        <li class="nav-item  ">
+                            <a href="{{ route('transaction-manager.listRequestPaid') }}" class="nav-link ">
+                                <span class="title">Yêu cầu thanh toán</span>
+                            </a>
+                        </li>
+                    @endif
                 </ul>
             </li>
 
